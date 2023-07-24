f8cd4c0e717cc1803c44f1032ba415b741883037
unknown <peter.ostertag99@gmail.com>
2023-07-23 21:33:21 -0500

    Added port and middleware

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
-const app express();
+const PORT = process.env.port || 3001;
+const app = express();
+const dbFilePath = path.join(__dirname, 'db.json');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
