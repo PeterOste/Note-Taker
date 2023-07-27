27e3b3142bf4576d46a4dd4afb99e93ab7b7110b
unknown <peter.ostertag99@gmail.com>
2023-07-27 16:35:18 -0500

    Populated the routes folder with data from server.js

---
+++ routes/apiRoutes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+const router = express.Router();
+const dbFilePath = path.join(__dirname, '..', 'db', 'db.json');
+
+// Route that retrieves an existing note.
+router.get('/notes', (req, res) => {
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to correctly read note.' });
+        }
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
+});
+
+// Route that saves a new note.
+router.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (!title || !text) {
+      return res.status(400).json({ error: 'Title and text are required.' });
+    }
+
+    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to correctly read note.' });
+      }
+      
+      const notes = JSON.parse(data);
+      const newNote = {
+        id: uuidv4(),
+        title,
+        text,
+      };
+      
+      // Adds new note to notes array.
+      notes.push(newNote);
+      
+      fs.writeFile(dbFilePath, JSON.stringify(notes, null, 2), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Failed to correctly save note.' });
+        }
+        
+        // The notes array is sent as a response, including the new note.
+        res.json(newNote);
+      });
+    });
+});
+
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,3 +2,10 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+
+// Route that serves the index.html.
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
+module.exports = router;
