9522acae019555e4f917887d7e3b7c3b743a1ad4
unknown <peter.ostertag99@gmail.com>
2023-08-02 18:17:23 -0500

    Added notes.html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,4 +8,9 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
+// Route that serves the notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 
-const PORT = process.env.port || 3003;
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 // Middleware.
